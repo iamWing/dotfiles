6af47cc1b102512583287d4ffc73d4b91ac0a3c5
Wing Chau <wing@devtography.com>
2022-08-23 06:18:20 +0000

    Create `install.sh`
    - Included function `create_symlinks` to link dot files to `$HOME`.

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+set -e
+
+# cd to script's directory
+cd "$(dirname "$(readlink -f -- "$0")")"
+
+CWD=$(pwd) # Set current working directory
+
+
+create_symlinks() {
+  # Get a list of all dot files in the working directory.
+  dotfiles="$(find "$CWD" -maxdepth 1 -type f -name ".*")"
+
+  for file in $dotfiles; do
+    filename="$(basename "$file")"
+    echo "Creating symlink to $filename in home directory."
+    ln -s "$CWD/$filename" ~/"$filename"
+  done
+}
+
+# shellcheck source=scripts/vim-setup.sh
+. "$CWD"/scripts/vim-setup.sh
+
+create_symlinks
