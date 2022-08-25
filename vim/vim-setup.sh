a2c38bc30549cba49362bfc18ab412f4eeb55f84
Wing Chau <wing@devtography.com>
2022-08-25 18:41:05 +0000

    Link `.vimrc` to `$HOME`
    - Config script to link the included `.vimrc` to `$HOME`.
    - Added `FORCE` mode to overwite existing files.
    - Added relevant test.

--- vim/vim-setup.sh
+++ vim/vim-setup.sh
@@ -9,15 +9,19 @@ ROOT=$(dirname "$(pwd)")
 # shellcheck source=utils/printf-helpers.sh
 . "$ROOT"/utils/printf-helpers.sh
 
+FORCE=false
+
 usage() {
-  printf "Usage: %s [-v|h]\n\n" "$0"
+  printf "Usage: %s [-f|h|v]\n\n" "$0"
   printf "OPTIONS:\n"
+  printf "  -f\t\tforce mode; overwrites existing files\n"
   printf "  -v\t\tverbose mode\n"
   printf "  -h\t\thelp\n"
 }
 
-while getopts "hv" opt; do
+while getopts "fhv" opt; do
   case "$opt" in
+    f) FORCE=true ;;
     h)
       usage
       exit 0
@@ -31,14 +35,42 @@ while getopts "hv" opt; do
 done
 
 # Setup `onedark.vim` theme
-verbose_print "Setting up VIM theme 'onedark.vim'...\n"
+verbose_print "> Setting up VIM theme 'onedark.vim'...\n\n"
+
+ONEDARK_PATH="$HOME"/.vim/pack/plugins/opt/onedark.vim
+
+if [ -e "$ONEDARK_PATH" ]; then
+  verbose_print "Existing 'onedark.vim' found at '%s'\n" "$ONEDARK_PATH"
+  if [ "$FORCE" = false ]; then
+    verbose_print "Aborting...\n\n"
+    exit 1
+  fi
+
+  verbose_print "Removing existing 'onedark.vim' at '%s'...\n" "$ONEDARK_PATH"
+  rm -rf "$ONEDARK_PATH"
+fi
 mkdir -p "$HOME"/.vim/pack/plugins/opt
-git clone https://github.com/joshdick/onedark.vim.git \
-  "$HOME"/.vim/pack/plugins/opt/onedark.vim
-verbose_print "DONE\n"
+git clone https://github.com/joshdick/onedark.vim.git "$ONEDARK_PATH"
+verbose_print "DONE\n\n"
+
+# Link `.vimrc` to `$HOME`
+verbose_print "> Linking '.vimrc' to '%s/.vimrc'...\n\n" "$HOME"
+if [ -e "$HOME"/.vimrc ]; then
+  verbose_print "'.vimrc' exists at '%s/.vimrc'\n" "$HOME"
+  if [ "$FORCE" = false ]; then
+    verbose_print "Aborting...\n\n"
+    exit 1
+  fi
+
+  verbose_print "Removing existing '.vimrc' at '%s/.vimrc'\n" "$HOME"
+  rm -rf "$HOME"/.vimrc
+fi
+verbose_print "Creating symlink...\n"
+ln -s "$SCRIPT_PATH"/.vimrc "$HOME"/.vimrc
+verbose_print "DONE\n\n"
 
 # Cleanup
-unset CWD ROOT
+unset CWD ROOT ONEDARK_PATH
 unset usage
 
 # Not unset VERBOSE as this variable is intended to be shared across
--- vim/vim-setup.test.sh
+++ vim/vim-setup.test.sh
@@ -5,7 +5,7 @@ printf "> %s" "$0"
 printf " %s" "$@"
 printf "\n> Test script 'vim/vim-setup.sh'\n\n"
 
-# cd to repo's root directory
+# cd to script's directory
 cd "$(dirname "$(readlink -f -- "$0")")" || exit 1
 CWD=$(pwd)
 ROOT=$(dirname "$(pwd)")
@@ -17,9 +17,9 @@ ROOT=$(dirname "$(pwd)")
 MOCK=false
 CLEANUP=false
 
-task_done() ( printf_green -- "- Done\n\n" )
+task_done() ( printf_green -- "- Done\n" )
 
-test_passed() ( printf_green "%s Passed\n\n" "-" )
+test_passed() ( printf_green "%s Passed\n" "-" )
 
 test_failed() { 
   _fail_msg="$1"
@@ -39,14 +39,24 @@ usage() {
 cleanup() {
   if [ "$MOCK" = true ]; then
     if [ -e "$ROOT"/tmp/.vim ]; then 
-      printf "Removing existing file(s) on path '%s/tmp/.vim'...\n" "$ROOT"
+      printf "Removing existing file(s) on path '%s/tmp/.vim'...\t" "$ROOT"
       rm -rf "$ROOT"/tmp/.vim
+      task_done
+    fi
+    if [ -e "$ROOT"/tmp/.vimrc ]; then
+      printf "Removing existing '.vimrc' on path '%s/tmp/.vimrc'...\t" "$ROOT"
+      rm -rf "$ROOT"/tmp/.vimrc
       task_done
     fi
   else
     if [ -e "$DIR_ONEDARK" ]; then
-      printf "Removing existing file(s) on path '%s'...\n" "$DIR_ONEDARK"
+      printf "Removing existing file(s) on path '%s'...\t" "$DIR_ONEDARK"
       rm -rf "$DIR_ONEDARK"
+      task_done
+    fi
+    if [ -e "$HOME"/.vimrc ]; then
+      printf "Removing existing '.vimrc' on path '%s'...\t" "$PATH_VIMRC"
+      rm -rf "$HOME"/.vimrc
       task_done
     fi
   fi
@@ -68,14 +78,22 @@ while getopts "chm" opt; do
 done
 
 # Set up
-if [ "$MOCK" = true ]; then HOME="$ROOT"/tmp; fi
+printf ": Set up :\n"
+if [ "$MOCK" = true ]; then 
+  printf "Mocking '\$HOME' path as '%s/tmp'...\t" "$ROOT"
+  HOME="$ROOT"/tmp
+  task_done
+fi
 
 DIR_ONEDARK="$HOME"/.vim/pack/plugins/opt/onedark.vim
+PATH_VIMRC="$HOME"/.vimrc
 
 cleanup
 
 
 # Test
+printf "\n: Execute test cases :\n"
+
 printf "Executing script '%s/vim/vim-setup.sh'...\n" "$ROOT"
 # shellcheck source=vim/vim-setup.sh
 if ! err=$(. "$CWD"/vim-setup.sh 2>&1 1>/dev/null); then
@@ -105,7 +123,17 @@ fi
 test_passed
 unset err
 
+printf "Checking is '.vimrc' exists on path '%s'...\n" "$PATH_VIMRC"
+if [ ! -f "$PATH_VIMRC" ]; then
+  test_failed "'.vimrc' not found on expected path '%s'.\n" "$PATH_VIMRC"
+  exit 1
+fi
+test_passed 
+
 # Cleanup
+printf "\n: Clean up :\n"
 if [ "$CLEANUP" = true ]; then cleanup; fi
 
 unset CWD ROOT MOCK CLEANUP
+unset task_done task_passed task_failed usage
+unset DIR_ONEDARK PATH_VIMRC
